2a8bb866c9a4008aa3302722e49f562f421293e2
Rajdeep Singh Hundal <rajdeep@Rajdeeps-Laptop.local>
2024-03-09 21:57:35 +0800

    remove lab

--- ta-copy/cs4218-lab-5/src/main/java/cs4218/interfaces/apps/EchoApplication.java
+++
@@ -1,9 +0,0 @@
-package cs4218.interfaces.apps;
-
-import cs4218.exceptions.EchoException;
-
-import java.util.List;
-
-public interface EchoApplication extends Application {
-    void run(List<String> tokens) throws EchoException;
-}
