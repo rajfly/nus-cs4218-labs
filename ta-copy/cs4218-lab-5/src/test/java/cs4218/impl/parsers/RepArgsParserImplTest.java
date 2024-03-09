8d16a1859506301f90f3af07b1b5b68c439a04ad
Rajdeep Singh Hundal <rajdeep@Rajdeeps-Laptop.local>
2024-03-09 21:49:24 +0800

    added lab 5

---
+++ ta-copy/cs4218-lab-5/src/test/java/cs4218/impl/parsers/RepArgsParserImplTest.java
@@ -0,0 +1,70 @@
+package cs4218.impl.parsers;
+
+import cs4218.enums.REPLACE_MODE;
+import cs4218.exceptions.RepException;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.EnabledOnOs;
+import org.junit.jupiter.api.condition.OS;
+
+import java.nio.file.Path;
+import java.util.Arrays;
+import java.util.Collections;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+class RepArgsParserImplTest {
+    RepArgsParserImpl parser;
+
+    @BeforeEach
+    void setUp() {
+        parser = new RepArgsParserImpl();
+    }
+
+    @Test
+    void getMode_WithFlag_ReplaceCharactersModeSelected() throws RepException {
+        parser.parseAndValidate(Arrays.asList("-c", "pattern", "replacement", "path"));
+
+        assertEquals(REPLACE_MODE.REPLACE_CHARACTERS, parser.getMode());
+    }
+
+    @Test
+    @EnabledOnOs({OS.LINUX, OS.WINDOWS, OS.MAC})
+        // Check for OS specific bugs
+    void parseAndValidate_EmptyString_ThrowsRepException() {
+        assertThrows(RepException.class, () -> parser.parseAndValidate(Collections.emptyList()));
+    }
+
+    @Nested // Similar tests can be grouped together (same method or setup or ...)
+    class TypicalValues {
+        @BeforeEach
+        void setUp() throws RepException {
+            String filePath = Path.of("file-path", "to", "file.txt").toString();
+            parser.parseAndValidate(Arrays.asList("hello", "world", filePath));
+        }
+
+        @Test
+        void getPattern_TypicalValues_ReturnsCorrectValue() {
+            String pattern = parser.getPattern();
+
+            assertEquals("hello", pattern);
+        }
+
+        @Test
+        void getReplacement_TypicalValues_ReturnsCorrectValue() {
+            String replacement = parser.getReplacement();
+
+            assertEquals("world", replacement);
+        }
+
+        @Test
+        void getFilePath_TypicalValues_ReturnsCorrectValue() {
+            String filePath = parser.getFilePath();
+
+            String expected = Path.of("file-path", "to", "file.txt").toString();
+            assertEquals(expected, filePath);
+        }
+    }
+}
